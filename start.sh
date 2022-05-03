9f2b46d3128de15aa4d854153bc35769ffd75dcd
Arturo Montieri <montieri@gmail.com>
2022-05-03 11:38:19 +0200

    #SD-255 Errors management during start/stop

--- start.sh
+++ start.sh
@@ -7,13 +7,21 @@ echo "  *                                                                       
 echo "  *********************************************************************************"
 
 echo "  Starting DAFNE..."
+
 docker-compose up -d
-echo "  Application started"
 
-echo "  You can check the application status by means of the following command:"
-echo "  docker ps"
+return_code=$?
 
-echo "  You can check the application logs by means of the following commands:"
-echo "  docker logs be -f"
-echo "  docker logs fe -f"
-echo "  docker logs db -f (only in case of dockerized DB)"
+if [ $return_code -eq 0 ]; then
+    echo "  Application started"
+
+    echo "  You can check the application status by means of the following command:"
+    echo "  docker ps"
+
+    echo "  You can check the application logs by means of the following commands:"
+    echo "  docker logs be -f"
+    echo "  docker logs fe -f"
+    echo "  docker logs db -f (only in case of dockerized DB)"
+else
+    echo "  Application launching error!"
+fi
